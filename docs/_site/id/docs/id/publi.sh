a2f62389a8d51000b679162c38939b29249e8476
scyoon <scyoon@stealien.com>
2021-10-13 21:25:32 +0900

    [Edit] docs/ update

--- docs/_site/id/docs/id/publi.sh
+++
@@ -1,7 +0,0 @@
-# git checkout master
-rm -rf _site/
-bundle exec jekyll build
-git add --all
-git commit -m "`date`"
-git push origin master
-git subtree push --prefix=_site/ origin master
--- docs/publi.sh
+++ docs/publi.sh
@@ -1,6 +1,7 @@
-# git checkout master
+git checkout master
 rm -rf _site/
 bundle exec jekyll build
+cp -r _site docs
 git add --all
 git commit -m "`date`"
 git push origin master
